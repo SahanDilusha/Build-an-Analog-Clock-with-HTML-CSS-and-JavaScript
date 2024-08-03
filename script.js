58b48375d6be5f376bbdc2a463358dc415a46dec
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-03 21:02:45 +0530

    add script

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const hour = document.querySelector(".hour");
-const minute = document.querySelector(".minute");
-const second = document.querySelector(".second");
+const hourhand = document.querySelector(".hour");
+const minutehand = document.querySelector(".minute");
+const secondhand = document.querySelector(".second");
+
+const setTime = () => {
+
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const secondsDe = (second / 60) * 360 + 90;
+    const minutesDe = (minute / 60) * 360 + (second / 60) * 6 + 90;
+    const hoursDe = (hour / 12) * 360 + (minute / 60) * 30 + 90;
+    
+
+
+}
