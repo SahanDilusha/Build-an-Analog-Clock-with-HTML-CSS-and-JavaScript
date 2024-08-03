cf2806c717ab84d2d2031a390d21bcb9a90cdb88
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-03 21:17:08 +0530

    add google fonts

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const hourhand = document.querySelector(".hour");
 const minutehand = document.querySelector(".minute");
 const secondhand = document.querySelector(".second");
 
-const setTime = () => {
+function setTime() {
+
+
 
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
-    const secondsDe = (second / 60) * 360 + 90;
-    const minutesDe = (minute / 60) * 360 + (second / 60) * 6 + 90;
-    const hoursDe = (hour / 12) * 360 + (minute / 60) * 30 + 90;
-    
+    const secondsDe = (seconds / 60) * 360 + 90;
+    const minutesDe = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+    const hoursDe = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
 
+    hourhand.style.transform = `rotate(${hoursDe}deg)`;
+    minutehand.style.transform = `rotate(${minutesDe}deg)`;
+    secondhand.style.transform = `rotate(${secondsDe}deg)`;
 
 }
+
+setInterval(setTime, 1000);
+setTime();
